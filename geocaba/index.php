20b37c02d4763c645d7ba3a9fcc821c9432a59a3
XtnPaez <cristian.paez@bue.edu.ar>
2025-08-26 13:26:13 -0300

    Actualizo index.php

--- geocaba/index.php
+++ geocaba/index.php
@@ -4,8 +4,8 @@
     // traer_datos_utiles();
     // traer_datos_de_catastro();
     // traer_coordenadas_gkba();
-    traer_coordenadas_wgs84();
+    // traer_coordenadas_wgs84();
     // crear_geom();
     // wgs84_para_cuis();
-    // crear_geom_para_cuis();
+    crear_geom_para_cuis();
 ?>
